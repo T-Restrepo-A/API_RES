29fda77b9f0fcd14a0de7cf281f4d0b9b5cde6d0
T-Restrepo-A <tomas.res.ald@gmail.com>
2023-10-29 14:17:14 -0500

    Se prueba la conexion

--- app.js
+++ app.js
@@ -31,36 +31,3 @@ conexion.connect(function(error){
    }
 });
 
-app.get('/api_res/tbl_productos', (req,res)=>{
-    conexion.query('SELECT * FROM tbl_productos', (error,filas)=>{
-        if(error){
-            throw error;
-        }else{
-            res.send(filas);
-        }
-    });
-});
-
-app.get('/api_res/tbl_productos/:id', (req,res)=>{
-    conexion.query('SELECT * FROM tbl_productos WHERE id=?', [req.params.id] , (error,fila)=>{
-        if(error){
-            throw error;
-        }else{
-           res.send(fila);
-           res.send(fila[0].descripción);  // para traer un solo resgistro “ descripción”
-        }
-    });
-});
-
-app.post('/api_res/tbl_productos',(req,res)=>{
-    let data = {descripcion:req.body.descripcion, costo:req.body.costo};
-    let sql = "INSERT INTO tbl_productos SET ?";
-    conexion.query(sql, data, (error,results)=>{
-        if(error){
-            throw error;
-        }else{
-           res.send(results);
-        }
-    });
-});
-
